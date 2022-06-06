465da33c8a64f11b77828fb4c62c16e3940666ac
MicaelaCuan2 <101810478+MicaelaCuan2@users.noreply.github.com>
2022-06-05 19:56:04 -0700

    ter

---
+++ Assets/Ejercicio2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ejercicio2 : MonoBehaviour
+{
+    public Text TextIngresado;
+    public Text Texto;
+    public bool VacioONo;
+    void Start()
+    {
+        VacioONo = true;
+    }
+    public void ShowMessage()
+    {
+        if (VacioONo == true)
+        {
+            Debug.Log("Error, ingrese texto");
+        }
+        else if (VacioONo == false)
+        {
+            TextIngresado.text = Texto.text;
+        }
+    }
+}
